35140de1a7bef688882a7886726637c0bb4e11c4
SCORPION_MD <joelsmith228kpevika@gmail.com>
2025-09-20 09:27:28 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
+module.exports = {
+  owner: process.env.OWNER_NUMBER || '+22800000000',
+  botName: process.env.BOT_NAME || 'ZEUS_X',
+  sessionFile: process.env.SESSION_FILE || 'session.json',
+  prefix: '!',  // préfixe pour les commandes
+}
 
