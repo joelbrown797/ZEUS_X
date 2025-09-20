e6b5396eb8726f4dcc0338bbf34b1da6b008863b
SCORPION_MD <joelsmith228kpevika@gmail.com>
2025-09-20 09:42:34 +0000

    Update like.js

--- commands/like.js
+++ commands/like.js
@@ -1,1 +1,8 @@
+module.exports = {
+  run: async (sock, msg, args) => {
+    const emojis = ['❤️', '🔥', '👍', '😂', '😍', '💯']
+    const emoji = emojis[Math.floor(Math.random() * emojis.length)]
+    await sock.sendMessage(msg.key.remoteJid, { react: { text: emoji, key: msg.key } })
+  }
+}
 
