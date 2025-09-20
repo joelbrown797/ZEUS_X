320268bc83a2e297042f0a65720c413c45a0c876
SCORPION_MD <joelsmith228kpevika@gmail.com>
2025-09-20 09:37:46 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  run: async (sock, msg, args) => {
+    const helpText = `
+🤖 *ZEUS_X - Commandes disponibles*:
+• !ping - Test de connexion
+• !like - Like le statut
+• !regarder - Regarde un statut
+• !ouvrir - Ouvre vues uniques
+• !help - Affiche ce menu
+    `
+    await sock.sendMessage(msg.key.remoteJid, { text: helpText })
+  }
+}
